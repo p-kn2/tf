6935f94f9e9980c85999f3104033a26210647fe0
p-kn2 <piyush.dev0325@gmail.com>
2026-02-25 18:38:45 +0000

    test

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,5 +1,5 @@
 variable "region" {
-    description = "The AWS region to deploy resources in"
+    description = "Thee AWS region to deploy resources in"
     type = string
     default = "us-west-2"
 }
