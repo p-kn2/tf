1ceb6351a7d0abb60926a30ae682142549401e29
p-kn2 <piyush.dev0325@gmail.com>
2026-02-25 18:38:10 +0000

    test

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,3 +1,9 @@
+variable "region" {
+    description = "The AWS region to deploy resources in"
+    type = string
+    default = "us-west-2"
+}
+
 terraform {
     required_providers {
         aws = {
@@ -8,7 +14,7 @@ terraform {
 }
 
 provider "aws" {
-    region = "us-west-2"
+    region = var.region
 }
 
 resource "aws_instance" "myserver" {
